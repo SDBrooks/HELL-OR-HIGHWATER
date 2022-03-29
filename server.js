d3bbd7b62c6ef31d1aeff634a5ceab9a740b66cf
Shane Brooks <shanedbrooks06@gmail.com>
2022-03-29 14:51:31 -0400

    Server Up

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3000
 const db = require('./db')
+const { Product } = require('./models')
+const res = require('express/lib/response')
 
 const app = express()
 
@@ -13,6 +15,23 @@ app.use(logger('dev'))
 app.get('/', (req, res) => {
   res.send('HELL OR HIGHWATER!')
 })
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find()
+    res.json(products)
+  })
+
+  app.get('/products/:id', async (req, res) => {
+    try {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    if (!product) throw Error('Product not found')
+    res.json(product)
+  } catch (e) {
+      console.log(e) 
+          res.send('Product not found!')
+  }
+ })
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
